783a856745ac36cd0cdd008a1d4d4b3fe6d14c5a
Sidharth Narayanan <sidharth.narayanan@live.com>
2023-01-15 16:59:54 -0600

    cleanup

--- src/main/java/frc/robot/implementation/DriveControllerImpl.java
+++ src/main/java/frc/robot/implementation/DriveControllerImpl.java
@@ -1,13 +1,6 @@
 package frc.robot.implementation;
 
-import edu.wpi.first.math.trajectory.Trajectory;
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.RamseteCommand;
-import edu.wpi.first.math.controller.PIDController;
-import edu.wpi.first.math.controller.RamseteController;
-import edu.wpi.first.math.controller.SimpleMotorFeedforward;
 import frc.robot.interfaces.DriveController;
-import frc.robot.main.Constants.*;
 
 public class DriveControllerImpl implements DriveController {
     private boolean moving = false;
@@ -28,31 +21,6 @@ public class DriveControllerImpl implements DriveController {
         System.out.println("Moving at speed:" + speed + ", rotation:" + rotation);
         moving = true;
         driveSubsystem.arcadeDrive(speed, rotation);
-    }
-
-    @Override
-    public Command getAutonomousCommand(Trajectory trajectory) {
-        RamseteCommand ramseteCommand =
-            new RamseteCommand(
-                trajectory,
-                driveSubsystem::getPose,
-                new RamseteController(AutoConstants.kRamseteB, AutoConstants.kRamseteZeta),
-                new SimpleMotorFeedforward(
-                    DriveConstants.ksVolts,
-                    DriveConstants.kvVoltSecondsPerMeter,
-                    DriveConstants.kaVoltSecondsSquaredPerMeter),
-                DriveConstants.kDriveKinematics,
-                driveSubsystem::getWheelSpeeds,
-                new PIDController(DriveConstants.kPDriveVel, 0, 0),
-                new PIDController(DriveConstants.kPDriveVel, 0, 0),
-                // RamseteCommand passes volts to the callback
-                driveSubsystem::tankDriveVolts,
-                driveSubsystem);            
-    
-        // Reset odometry to the starting pose of the trajectory.
-        //driveSubsystem.resetOdometry(trajectory.getInitialPose());
-
-        return ramseteCommand.andThen(() -> driveSubsystem.tankDriveVolts(0, 0));
     }
 
     @Override
--- src/main/java/frc/robot/interfaces/DriveController.java
+++ src/main/java/frc/robot/interfaces/DriveController.java
@@ -1,11 +1,7 @@
 package frc.robot.interfaces;
 
-import edu.wpi.first.math.trajectory.Trajectory;
-import edu.wpi.first.wpilibj2.command.Command;
-
 public interface DriveController {
     public void move(double distance, double speed, double rotation);
     public void move(double speed, double rotation);
     public void stop();
-    public Command getAutonomousCommand(Trajectory trajectory);
 }
